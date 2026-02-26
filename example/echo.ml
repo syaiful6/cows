c556382826564a0f5419eac354d3e2223b2428be
Syaiful Bahri <syai.bahri6@gmail.com>
2026-02-26 22:02:38 +0700

    feat: use logs library in echo example

--- example/echo.ml
+++ example/echo.ml
@@ -13,11 +13,15 @@ let rec handler conn =
   | `Close (code, reason) -> Cows.close ~code ~reason conn
 
 let () =
+  Logs_threaded.enable ();
+  Fmt_tty.setup_std_outputs ();
+  Logs.set_level (Some Logs.Debug);
+  Logs.set_reporter (Logs_fmt.reporter ());
   Eio_main.run @@ fun env ->
   Eio.Switch.run @@ fun sw ->
   let addr = `Tcp (Eio.Net.Ipaddr.V4.loopback, 9001) in
   let socket = Eio.Net.listen ~reuse_addr:true ~backlog:256 ~sw env#net addr in
-  Printf.printf "Listening on ws://127.0.0.1:9001\n%!";
+  Logs.info (fun f -> f "Listening on ws://127.0.0.1:9001\n%!");
   let server =
     Cohttp_eio.Server.make_response_action
       ~callback:(fun _conn req _body -> Cows.upgrade req handler)
